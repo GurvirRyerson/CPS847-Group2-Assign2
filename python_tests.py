c1f85a978c708a1f0c9902aceff0da9dee8c9be4
gurvirRyerson <gurvirsingh9612@gmail.com>
2018-02-16 08:49:30 -0500

    Added testing and coverage report

---
+++ python_tests.py
@@ -0,0 +1,20 @@
+import unittest
+from code_to_test import multiply_numbers, sum_numbers
+
+
+class SimpleTests(unittest.TestCase):
+	def test_sum(self):
+		list_of_nums = []
+		for i in range(0,9):
+			for j in range(0,9):
+				self.assertEqual(sum_numbers(list_of_nums), sum(list_of_nums))
+				list_of_nums.append(j)
+
+	def test_multiply(self):		
+		for i in range(0,9):
+			for j in range(0,9):
+				self.assertEqual(multiply_numbers(i,j), i*j)
+
+
+if __name__ == "__main__":
+	unittest.main()
